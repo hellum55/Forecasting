0af5420d6c84ab41d1acb9ab8a2e8247c8af05ac
hellum55 <hellum.christian@gmail.com>
2024-01-20 08:50:25 +0100

    b

--- Old exams/Reexam 2023.R
+++ Old exams/Reexam 2023.R
@@ -40,20 +40,41 @@ fit <- lm(y ~ week) #fits a linear model (OLS)
 summary(fit)
 
 #Question 3:
-plot(y)
-fit1 <- lm(y ~ log(trend) + week) #fits a linear model (OLS)
+hist(x)
+fit1 <- lm((y) ~ (-1/(trend)+week)) #fits a linear model (OLS)
 summary(fit1)
 
-plot(fit1$fitted.values)
-lines(fit1$fitted.values, col = "red")
+plot((y), type="l")
+lines(fit1$fitted.values, col="red")
 
 plot(resid(fit1))
 lines(resid(fit1), col = "red")
+accuracy(ts(fit1$fitted.values, frequency = 4),y)
 
 res<-fit1$residuals
-jarque.bera.test(res) 
+jarque.bera.test(res) #Null = data is normally distributed
 acf(res)
 dwtest(fit1)
+
+multi <- decompose(y, type = "multiplicative")
+plot(multi)
+S<-multi$seasonal
+
+SI <- scale(S[1:4]) #the indicies are the same for repeated season, so only need 12 of them as have 12 different months
+plot(SI, type="h") 
+
+CMA <- ma(y, order=52, centre=TRUE) #desesonalized data
+#seasonal facto
+SF<-y/CMA
+plot(SF, type="l")
+abline(h=1, col = "red")
+
+linmod <- lm(CMA ~ trend, na.action = "na.exclude")
+CMAT <- linmod$fitted.values
+#cycle factor
+Cycle <- na.exclude(CMA) / CMAT
+ts.plot(Cycle)
+abline(h=1, col = "red") 
 
 #Question 4:
 fit2 <- auto.arima(y)
@@ -123,13 +144,14 @@ accuracy(fcast4, outsamp_y)
 
 #Question 8:
 #group data for further analysis
-
-z <- ts(cbind(x,y), frequency = 4)
-dz<- diff(z)
+dy <- diff(y)
+dx <- diff(x)
+z <- ts(cbind(dy,dx), frequency = 4)
+#dz<- diff(z)
 
 #determine the order of VAR
-VARselect(dz, lag.max = 8, type="const")[["selection"]] #optimal p =1
-var1 <- VAR(dz, p=4, type="const") #estimate VAR(1)
+VARselect(z, lag.max = 8, type="const")[["selection"]] #optimal p =1
+var1 <- VAR(z, p=4, type="const") #estimate VAR(1)
 summary(var1)
 
 #Can do model diagnostics as in AR models
@@ -147,11 +169,8 @@ roots(var1)
 plot(irf(var1,boot = TRUE, ci=0.95))
 
 #split the sample into training and test
-insamp<-ts(y[1:136], frequency=4) #90% of the data
-outsamp <-y[137:length(y)]
-
-insampdz <- ts(dz[1:136, 1:2], frequency = 4)
-outsampdz <- ts(dz[137:181, 1:2], frequency = 4)
+insampdz <- ts(z[1:136, 1:2], frequency = 4)
+outsampdz <- ts(z[137:181, 1:2], frequency = 4)
 
 #select the order of VAR
 VARselect(insampdz, lag.max = 8, type="const")[["selection"]]
@@ -163,32 +182,44 @@ summary(fit)
 serial.test(fit, lags.pt=4, type="PT.asymptotic")
 
 #Obtain the forecast for the test period
+length(outsampdz)
 fcast3 <- forecast(fit, h=45)
 plot(fcast3) #provides a plot of the forecasts
 
 #calculate the accuracy of the forecast for the ltn variable
-accuracy(fcast3$forecast$y, outsampdz[1:45, 2])
-op <- fcast3$forecast$y
+accuracy(fcast3$forecast$dy, outsampdz[1:45, 1])
 
 #Aggregate data to levels
-fcast_y<-y[137]+cumsum(fcast3$forecast$y$mean)
+fcast_y<-y[137]+cumsum(fcast3$forecast$dy$mean)
 library(ggplot2)
-Date <- seq(1:length(y))
+Date <- seq(1:182)
 ggplot(data, aes(x=Date)) +
   geom_line(aes(y = append(y[1:137],fcast_y)), color = "red")+    
   geom_line(aes(y = y)) +
   scale_y_continuous(name = "")
 
 #Question 9:
-accuracy(fcast3$forecast$y, outsampdz[1:45, 2])
+accuracy(fcast3$forecast$dy, outsampdz[1:45, 1])
 accuracy(fcast4, outsamp_y)
 accuracy(fcast, outsamp)
 
 z1 <- ts(cbind(x,y), frequency = 4)
 dz1 <- (z)
-fit4 <- VAR(z, p=8, type="const")
+fit4 <- VAR(z, p=4, type="const")
 fcast8 <- forecast(fit4, h=4)
 plot(fcast8) #provides a plot of the forecasts
+
+fcast_full <- y[182]+cumsum(fcast8$forecast$dy$mean)
+Y<-ts(append(y, fcast_full), frequency = 4)
+# Now create the plot with the actual data and the forecast appended
+plot(y, main="Actual Data and Forecast", xlab="Time", ylab="Values", col="blue", xlim=c(start(y)[1], end(Y)[1]))
+lines(Y, col="red", lty=2)
+Y_subset <- window(Y, start=182, end = 186)
+lines(Y_subset, col="red", lty=2)
+legend("topleft", legend=c("Actual Data", "Forecast"), col=c("blue", "red"), lty=1:2, bty="n")
+
+
+
 
 fit4 <- Arima(y, model=fit3)
 summary(fit4) #preserving the estimates obtained in previous step 
